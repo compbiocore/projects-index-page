1b058e26c68b77ca5331207dbc50dd73a0f4594e
fernandogelin <fernandogelin@gmail.com>
2018-08-03 15:50:42 -0400

    chained promises to get readme content

--- index.js
+++ index.js
@@ -8,26 +8,12 @@ const token = process.env.GITHUB_TOKEN;
 const user = process.env.USER;
 const organization = 'compbiocore';
 
-const getRepos = (org) => {
-  return new Promise((resolve, reject) => {
-    request({
-      url: `https://api.github.com/orgs/${org}/repos`,
-      headers: {
-        'Authorization': `token ${token}`,
-        'User-Agent': user,
-        'Accept': 'application/vnd.github.v3+json'
-      }
-    }, (err, response) => {
-      const json = JSON.parse(response.body);
-      resolve(json);
-    });
-  });
-};
 
 const getRepoInfo = (org, repo, type) => {
   return new Promise((resolve, reject) => {
+
     request({
-      url: `https://api.github.com/repos/${org}/${repo}/${type}`,
+      url: `https://api.github.com/repos/${organization}/${repo}/${type}`,
       headers: {
         'Authorization': `token ${token}`,
         'User-Agent': user,
@@ -40,48 +26,48 @@ const getRepoInfo = (org, repo, type) => {
   });
 };
 
-const repoPromise = getRepos(organization);
+new Promise((resolve, reject) => {
 
-repoPromise.then((values) => {
-  return JSON.stringify(values.map((item) => item.name))
+    request({
+      url: `https://api.github.com/orgs/${organization}/repos`,
+      headers: {
+        'Authorization': `token ${token}`,
+        'User-Agent': user,
+        'Accept': 'application/vnd.github.v3+json'
+      }
+    }, (err, response) => {
+      const json = JSON.parse(response.body);
+      resolve(json);
+    });
+
+}).then((values) => {
+
+  const contentPromises = values.map((repo) => getRepoInfo(organization, repo.name, 'contents'))
+  return Promise.all(contentPromises)
+
+}).then((values) => {
+
+  let repos = [];
+  const contents = values.map((content) =>
+      content.map((file) => {
+      if (Object.values(file).indexOf("docs") > -1 ) {
+        repos.push(file.url.split("/")[5]);
+      }}));
+
+  return repos
+
+}).then((values) => {
+
+  const readmePromises = values.map((item) => getRepoInfo(organization, item, 'readme'))
+  return Promise.all(readmePromises)
+
+}).then((values) => {
+
+  return JSON.stringify(values.map((item) => Base64.decode(item.content)))
+
 }).then((str) => {
-  fs.writeFileSync('data/repos.json', str);
-  console.log("Data written to data/repos.json");
-});
 
-let rawdata = fs.readFileSync('data/repos.json');
-let repos = JSON.parse(rawdata);
-
-const readmePromises = repos.map((item) => getRepoInfo(organization, item, 'readme'));
-
-Promise.all(readmePromises).then((values) => {
-  return JSON.stringify(values, null, 2);
-}).then((str) => {
-  // values, do stuff with
   fs.writeFileSync('data/readmes.json', str);
-  // success case, the file was saved
   console.log('Data written to data/readmes.json');
+
 });
-
-
-const contentPromises = repos.map((item) => getRepoInfo(organization, item, 'contents'));
-
-Promise.all(contentPromises).then((values) => {
-  return JSON.stringify(values, null, 2);
-}).then((str) => {
-  // values, do stuff with
-  fs.writeFileSync('data/contents.json', str);
-  // success case, the file was saved
-  console.log('Data written to data/contents.json');
-});
-
-
-let readmes_raw = fs.readFileSync("data/readmes.json");
-let readmes = JSON.parse(readmes_raw);
-
-const getOverview = (readme) => {
-  const decoded_readme = Base64.decode(readme.content);
-  console.log(decoded_readme.split("\n"));
-}
-
-readmes.map((item) => getOverview(item));
